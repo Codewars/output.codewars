43e89831dd5a461462ee595dec7853d3afa0bc3d
jake hoffner <jake.hoffner@gmail.com>
2014-08-26 13:16:57 -0700

    added resize postMessage so that we can get height information from window

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,12 @@ window.open = $.noop
 window.addEventListener('message', function(e, b){
     if (e.origin.indexOf('codewars') > 0){
         $('body').html(e.data)
+        window.parent.postMessage($(window).height(), '*');
     }
+});
+
+$(window).resize(function(e)
+{
+    window.parent.postMessage($(window).height(), '*');
 });
 
